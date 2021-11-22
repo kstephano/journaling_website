0863e2576233f844f099546cbe88c45f8d13f712
Rahib <rrahman829@outlook.com>
2021-11-22 15:34:52 +0000

    created function to drawall posts in an array

--- client/index.js
+++ client/index.js
@@ -1,6 +1,5 @@
 const uuid = require('uuid');
 
-
 if (!uuid.validate(localStorage.getItem("id"))) {
     localStorage.removeItem("id")
     localStorage.setItem("id", uuid.v4())
@@ -34,29 +33,29 @@ document.querySelector("#greyed-out").addEventListener("click", () => {
     commentBox.style.zIndex = "-100"
 })
 
-async function init() {
-    // let res = await fetch('http//:localhost:3000/getposts')
-    // let data = await res.json()
-    let listOfPosts = templatePosts
+// async function init() {
+//     // let res = await fetch('http//:localhost:3000/getposts')
+//     // let data = await res.json()
+//     let listOfPosts = templatePosts
 
-}
+// }
 
-function createPost(postJSON) {
-    let postCard = document.createElement("div").classList.add("post-card")
-    
-}
+// function createPost(postJSON) {
+//     let postCard = document.createElement("div").classList.add("post-card")
 
-let templatePosts = [
-    {   
-        id: "96584286-4b00-48da-bc1d-fa1eb82a5ced",
-        time: 1637585802352,
-        title: "Post Heading",
-        body: {text: "Body text", gif: "https://c.tenor.com/58egLELFYTsAAAAM/vibing.gif"},
-        comments: [{id: "96584286-4b00-48da-bc1d-fa1eb82a5cee", time: 1637585812352, body: "comment 1"}, 
-        {id: "96584286-4b00-48da-bc1d-fa1eb82a5cea", time: 1637585813352, body: "comment 2"}],
-        emojis: {"emoji1": 0, "emoji2": 5, "emoji3": 11}
-    }
-]
+// }
+
+// let templatePosts = [
+//     {   
+//         id: "96584286-4b00-48da-bc1d-fa1eb82a5ced",
+//         time: 1637585802352,
+//         title: "Post Heading",
+//         body: {text: "Body text", gif: "https://c.tenor.com/58egLELFYTsAAAAM/vibing.gif"},
+//         comments: [{id: "96584286-4b00-48da-bc1d-fa1eb82a5cee", time: 1637585812352, body: "comment 1"}, 
+//         {id: "96584286-4b00-48da-bc1d-fa1eb82a5cea", time: 1637585813352, body: "comment 2"}],
+//         emojis: {"emoji1": 0, "emoji2": 5, "emoji3": 11}
+//     }
+// ]
 
 // // let currentUTCTimestap = currentLocalTimestap.
 // let date = Date.now();
--- client/model.js
+++ client/model.js
@@ -1,3 +1,4 @@
+
 let templatePost = {   
         id: "96584286-4b00-48da-bc1d-fa1eb82a5ced",
         time: 1637585802352,
@@ -5,27 +6,27 @@ let templatePost = {
         body: {text: "Body text", gif: "https://c.tenor.com/58egLELFYTsAAAAM/vibing.gif"},
         comments: [{id: "96584286-4b00-48da-bc1d-fa1eb82a5cee", time: 1637585812352, body: "comment 1"}, 
         {id: "96584286-4b00-48da-bc1d-fa1eb82a5cea", time: 1637585813352, body: "comment 2"}],
-        emojis: {"emoji1": 0, "emoji2": 5, "emoji3": 11}
+        emojis: {emoji1: 0, emoji2: 5, emoji3: 11}
 }
 
-let postArray = [templatePost]
+let postArray = [templatePost, templatePost, templatePost, templatePost, templatePost, templatePost, templatePost, templatePost, templatePost, templatePost]
 
 class Post {
     constructor(data) {
         this.id = data.id
         this.timestamp = data.timestamp
         this.title = data.title;
-        this.body = data.body;
-        this.gifUrl = data.gifUrl;
+        this.body = data.body.text;
+        this.gifUrl = data.body.gif;
         this.comments = data.comments;
-        this.numOfLikeReacts = data.numOfLikeReacts;
-        this.numOfLoveReacts = data.numOfLoveReacts;
-        this.numOfLaughReacts = data.numOfLaughReacts;
+        this.emoji1 = data.emojis.emoji1;
+        this.emoji2 = data.emojis.emoji2;
+        this.emoji3 = data.emojis.emoji3;
     }
 
     static get all() {
-        const entries = postArray.map((data) => new Post(data));
-        return entries;
+        const posts = postArray.map((data) => new Post(data));
+        return posts;
     }
 
     static findById(id) {
@@ -38,7 +39,7 @@ class Post {
         }
     }
 
-    draw() {
+    get draw() {
         let postCard = document.createElement("div")
         postCard.classList.add("post-card")
         let postTop = document.createElement("div")
@@ -64,21 +65,46 @@ class Post {
         let commentsButton = document.createElement("div")
         commentsButton.classList.add("comments-button")
         
+        let postCardList = [postTop, postBody, postBottom]
         let postBodyList = [postBodyGif, postBodyText]
         let postBottomList = [emojisContainer, commentsButton]
+        let emojisContainerList = [emojiButton1, emojiButton2, emojiButton3]
 
         postHeading.textContent = this.title
         postBodyGif.src = this.gifUrl
         postBodyText.textContent = this.body
-        emojiButton1.textContent = this.numOfLikeReacts
-        emojiButton2.textContent = this.numOfLoveReacts
-        emojiButton3.textContent = this.numOfLaughReacts
+        emojiButton1.textContent = this.emoji1
+        emojiButton2.textContent = this.emoji2
+        emojiButton3.textContent = this.emoji3
         commentsButton.textContent = "comments"
         commentsButton.id = this.id
 
-        let gallery = document.querySelector('gallery')
+        let gallery = document.querySelector('#gallery')
         gallery.append(postCard)
+        postCardList.forEach(element => {
+            postCard.append(element)
+        })
+        postTop.append(postHeading)
+        postBodyList.forEach(element => {
+            postBody.append(element)
+        })
+        postBottomList.forEach(element => {
+            postBottom.append(element)
+        })
+        emojisContainerList.forEach(element => {
+            emojisContainer.append(element)
+        })
+    }
+
+    static drawAll(postsArray) {
+        let arr = Post.all
+        arr.forEach(post => {
+            post.draw
+        })
     }
 
 
 }
+
+
+Post.drawAll(postArray)
