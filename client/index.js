25247a4effda9c8d04061c9658149dee598e10fe
Rahib <rrahman829@outlook.com>
2021-11-22 10:54:11 +0000

    added browserify and created unique IDs for new visitors

--- client/index.js
+++ client/index.js
@@ -1,7 +1,21 @@
+const uuid = require('uuid');
+
+
+if (!uuid.validate(localStorage.getItem("id"))) {
+    localStorage.removeItem("id")
+    localStorage.setItem("id", uuid.v4())
+}
+
+console.log(uuid.v4())
+console.log(localStorage.getItem("id"))
+
 let commentButtons = document.querySelectorAll(".comments-button")
 let greyBox = document.querySelector("#greyed-out")
 let commentBox = document.querySelector('#comment-section')
 let emojiButtons = document.querySelectorAll(".emoji-button")
+let uniqueID = document.querySelector('#unique-id')
+
+uniqueID.textContent = `Your Unique ID is: ${localStorage.getItem("id")}`
 
 commentButtons.forEach(commentBut => commentBut.addEventListener("click", () => {
     greyBox.style.zIndex = "99"
