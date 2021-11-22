4d246c9d62f2540d72d3f491033e384332172485
Kelvin Stephano <kstephano@gmail.com>
2021-11-22 12:41:17 +0000

    updated test for updated Entry model

--- server/tests/readWrite.spec.js
+++ server/tests/readWrite.spec.js
@@ -5,7 +5,7 @@ describe('readFromFile', () => {
     it('should successfully read data from entries.json and populate the array in entries.js with data', () => {
         helpers.readFromFile();
         expect(entries[0]).toHaveProperty(
-            'id', 'timestamp', 'title', 'body', 'gifUrl', 'comments', 'numOfLikeReacts', 'numOfLoveReacts', 'numOfLaughReacts'
+            'id', 'timestamp', 'title', 'body', 'comments', 'emojis'
         );
     });
 });
