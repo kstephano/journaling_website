31f56620d092925307d1231ad786400d03952ee3
Kelvin Stephano <kstephano@gmail.com>
2021-11-24 17:30:09 +0000

    refactor and added classes to elements for styling

--- client/entryBundle.js
+++ client/entryBundle.js
@@ -838,31 +838,46 @@ function version(uuid) {
 var _default = version;
 exports.default = _default;
 },{"./validate.js":14}],16:[function(require,module,exports){
-let uuid = require('uuid');
-let searchBtn = document.querySelector("#search-icon");
-let gifCont = document.querySelector(".gif-scrolling-displayer");
+const uuid = require('uuid');
 
-const apiId = "q7OQqQiFkKI87Cb4JZTdmON0sNbDV2hy";
+const apiId = "q7OQqQiFkKI87Cb4JZTdmON0sNbDV2hy"; // ID to use to fetch data from Giphy
 
-let noGif = document.createElement("img");
+// initialise HTML elements as JS objects
+const searchBtn = document.querySelector("#search-icon");
+const gifCont = document.querySelector(".gif-scrolling-displayer");
+const form = document.querySelector("form");
+const newGif = document.querySelector("#gif");
+// create placeholder img element to hold chosen gifs
+const noGif = document.createElement("img");
 noGif.setAttribute("src", "./assets/gifs/gif_placeholder.gif");
 noGif.setAttribute("alt", "No GIF selection");
 noGif.setAttribute("id", "no-gif");
 noGif.style.width = "200px";
+gifCont.appendChild(noGif);
 
-noGifOpt();
-gifTrend(gifCont);
+gifTrend(gifCont); // add trending GIFs to the container
+initListeners(); 
 
-searchBtn.addEventListener("click", gifWindow);
-document.querySelector("form").addEventListener("submit", upload)
+/**
+ * Initialise listeners for the search button, submit button, and remove GIF button.
+ */
+function initListeners() {
+    searchBtn.addEventListener("click", gifWindow);
+    form.addEventListener("submit", upload);
+    noGif.addEventListener('click', e => {
+        newGif.setAttribute("src", "./assets/gifs/gif_placeholder.gif");
+        newGif.removeAttribute("value");
+    });
+}
 
-
-
-
-async function gifWindow(e) {
+/**
+ * Async function to reload gif container, gif placeholder img, and to fill
+ * the container with gifs if a search value has been entered.
+ */
+async function gifWindow() {
     clearDiv(gifCont);
-    noGifOpt();
-    let value = document.querySelector("#search-input").value;
+    gifCont.appendChild(noGif);
+    const value = document.querySelector("#search-input").value;
     console.log(value)
     if(value){
         await gifSearching(gifCont, value)
@@ -870,59 +885,96 @@ async function gifWindow(e) {
         await gifTrend(gifCont);
     }
 };
-
+/**
+ * Async function used to fetch 10 trending GIFs from Giphy and
+ * use them to populate the specified div.
+ * 
+ * @param {The div object to populate with GIFs} div 
+ */
 async function gifTrend(div) {
-    let response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiId}&rating=g&limit=10`);
-    let data = await response.json();
+    const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${apiId}&rating=g&limit=23`);
+    const data = await response.json();
     divBuilder(data, div);
 }
 
+/**
+ * Async function used to fetch 10 GIFs from Giphy that match
+ * the given search criteria given by the user.
+ * 
+ * @param {The div object to populate with GIFs} div 
+ * @param {The search query sent to Giphy to find relevant GIFs} str 
+ */
 async function gifSearching(div, str){
-    let response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiId}&rating=g&q=${str}&limit=10`);
-    let data = await response.json();
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${apiId}&rating=g&q=${str}&limit=23`);
+    const data = await response.json();
     divBuilder(data, div);
 }
 
+/**
+ * Populates the specified div with the results fetched from the Giphy API.
+ * 
+ * @param {Data retrieved from Giphy containing an array of data about each GIF} data 
+ * @param {The div that is populated with GIFs} div 
+ */
 function divBuilder(data, div){
-    let gifs = data.data;
+    const gifs = data.data;
 
     for(let i = 0; i < gifs.length; i++){
-        let gif = gifs[i].images.fixed_width;
-        let img = document.createElement('img');
+        const gif = gifs[i].images.fixed_width;
+        // creates each GIF inside a div and appends that to the main div
+        const img = document.createElement("img");
+        const gifDiv = document.createElement("div");
+        gifDiv.classList.add("div-gif-wrap");
         img.setAttribute("src", gif.url);
         img.setAttribute("alt", gifs[i].title);
         img.setAttribute("value", gifs[i].images.original.url);
         img.setAttribute("id", gifs[i].images.original.url)
-        div.appendChild(img);
-        let listen = document.getElementById(gifs[i].images.original.url);
+        gifDiv.appendChild(img);
+        div.appendChild(gifDiv);
+        // set an onClick listener for each appended gif
+        const listen = document.getElementById(gifs[i].images.original.url);
         listen.addEventListener("click", setGif);
     }
 }
 
+/**
+ * Clears the specified div, setting textContent to an empty string.
+ * 
+ * @param {The div to have its contents cleared} div 
+ */
 function clearDiv(div){
     div.textContent = "";
 }
 
+/**
+ * Sets the GIF as the same as the GIF clicked on by the user.
+ * 
+ * @param {Click event on a selected GIF} e 
+ */
 function setGif(e){
-    let gif = e.target;
-    let newGif = document.querySelector("#gif");
-    newGif.setAttribute("src", gif.src);
-    newGif.setAttribute("value", gif.id);
+    const selectedGif = e.target;
+    newGif.setAttribute("src", selectedGif.src);
+    newGif.setAttribute("value", selectedGif.id);
     // newGif.setAttribute("alt", gif.alt);
     // newGif.setAttribute("id", "selected")
     // div.appendChild(newGif);
 }
 
+/**
+ * Uploads the post data to the server.
+ * 
+ * @param {Click event on the upload button} e 
+ */
 async function upload(e) {
     e.preventDefault();
-    let gifSelect = document.querySelector("#gif").getAttribute("value");
-    console.log(gifSelect);
+    const chosenGifUrl = newGif.getAttribute("value");
+    console.log(chosenGifUrl);
 
-    let postData = {
+    const postData = {
         id: uuid.v4(), 
         timestamp: Date.now(), 
         title: document.querySelector("#title-input").value, 
-        body: {text: document.querySelector("#entry-content").value, gifUrl: gifSelect}
+        body: { text: document.querySelector("#entry-content").value, gifUrl: chosenGifUrl }
     }
     
 	const options = {
@@ -932,24 +984,15 @@ async function upload(e) {
 			"Content-Type": "application/json"
 		}
 	};
+    // Attempt a POST request to the server API
     try{
-	    await fetch("http://localhost:3000/update/create", options);
+	    await fetch("https://journaling-website.herokuapp.com/update/create", options);
         // let resdata = await response.json();
         // console.log(resdata);
-
     } catch(err) {
         console.log(err)
     }
 
-    location.href = "index.html"
-}
-
-function noGifOpt(){
-    gifCont.appendChild(noGif);
-    document.getElementById("no-gif").addEventListener('click', e => {
-        let newGif = document.querySelector("#gif");
-        newGif.setAttribute("src", "./assets/gifs/gif_placeholder.gif");
-        newGif.removeAttribute("value");
-    })
+    location.href = "index.html"; // change page back to home
 }
 },{"uuid":1}]},{},[16]);
