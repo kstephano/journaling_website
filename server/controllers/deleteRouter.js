859e2fced59de4a66843fd7f14d3b3740c844f09
Kelvin Stephano <kstephano@gmail.com>
2021-12-02 10:58:46 +0000

    staging

---
+++ server/controllers/deleteRouter.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const Entry = require('../models/Entry')
+const deleteRouter = express.Router()
+
+
+// delete entry 
+deleteRouter.delete('/:id', function (req, res) {
+  try{
+    let entry = Entry.findById(req.params.id)
+      Entry.deleteById(req.params.id)
+      res.sendStatus(204)
+  } catch(err) {
+    res.status(404).send(err.message);
+  }
+})
+
+module.exports = deleteRouter;
